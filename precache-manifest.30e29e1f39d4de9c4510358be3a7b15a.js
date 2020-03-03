fb1464bc43bf4760a0693255470c5a86f51e5e48
hungdd <duyhung01247@gmail.com>
2020-03-03 17:29:33 +0700

    Updates

---
+++ precache-manifest.30e29e1f39d4de9c4510358be3a7b15a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a3274154c4bc3c5f652e8c5ed6a82be",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f0d2f61b1084692eb0e4",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "a47cfecf44b7e5aacc5f",
+    "url": "/static/js/2.a17f2856.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.a17f2856.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f0d2f61b1084692eb0e4",
+    "url": "/static/js/main.e80cd640.chunk.js"
+  },
+  {
+    "revision": "72adb58b9f589a97cccf",
+    "url": "/static/js/runtime-main.c5044cd6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
